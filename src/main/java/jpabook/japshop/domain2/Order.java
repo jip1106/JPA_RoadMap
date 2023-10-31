9d7160174529eb10450eace40d3f72da51750981
jipark1106 <jipark1106@gmail.com>
2023-10-31 14:18:58 +0900

    í프록시와 연관관계 관리

--- src/main/java/jpabook/japshop/domain2/Order.java
+++ src/main/java/jpabook/japshop/domain2/Order.java
@@ -7,6 +7,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "ORDERS")
 public class Order extends BaseEntity{
@@ -14,14 +17,17 @@ public class Order extends BaseEntity{
     @Id @GeneratedValue
     @JoinColumn(name="MEMBER_ID")
     private Long id;
-    @OneToOne
+
+    //order를 저장 할때  delivery를 같이 저장한다-> cascade = CascadeType.ALL
+    @OneToOne(fetch=LAZY, cascade =  ALL)
     @JoinColumn(name="DELIVERY_ID")
     private Delivery delivery;
-    @ManyToOne
+    @ManyToOne(fetch= LAZY)
     @JoinColumn(name="MEMBER_ID")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    //order를 저장 할때  OrderItem 를 같이 저장한다-> cascade = CascadeType.ALL
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
