6f43fe5e73527767553a544e9cf8f935d16d59a0
jipark1106 <jipark1106@gmail.com>
2023-11-01 18:07:01 +0900

    값 타입

---
+++ src/main/java/jpabook/japshop/domain2/Address.java
@@ -0,0 +1,41 @@
+package jpabook.japshop.domain2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+//@Setter
+public class Address {
+    @Column(length = 10)
+    private String city;
+
+    @Column(length = 20)
+    private String street;
+
+    @Column(length = 5)
+    private String zipcode;
+
+    public String fullAddress(){
+        return this.getCity() + this.getStreet() + this.getZipcode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity())
+                && Objects.equals(getStreet(), address.getStreet())
+                && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
--- src/main/java/jpabook/japshop/domain2/Member.java
+++ src/main/java/jpabook/japshop/domain2/Member.java
@@ -11,15 +11,21 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+
 public class Member extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
+/*
+    Adddress 값 타입으로 변경
     private String city;
     private String street;
     private String zipcode;
+*/
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
